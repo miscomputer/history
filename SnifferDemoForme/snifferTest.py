62dc17523bf895e348b71375589b298ab9d59162
shenyi <sne1029@163.com>
2016-12-27 11:57:51 +0800

    初学解包 备存

--- SnifferDemoForme/snifferTest.py
+++ SnifferDemoForme/snifferTest.py
@@ -15,10 +15,11 @@ for _,pdata in p:
         # dport = tcp.dport
         if tcp.__class__.__name__ == 'TCP' and src_ip == '10.187.162.202' and len(tcp.data) > 1:
             dport = tcp.dport
-            # if dport == 443:
-            print '%s ---> %s:%s' % (src_ip, dst_ip, dport)
-            # headers = dpkt.http.parse_headers(tcp.data)
-            str_start = tcp.data.find('{')
-            str_end = str(str_start).find('\r\n')
-            json_str = tcp.data[str_start:str_end-1]
-            print json_str
+            sport = tcp.sport
+            if sport == 80:
+                print '%s ---> %s:%s' % (src_ip, dst_ip, dport)
+                # headers = dpkt.http.parse_headers(tcp.data)
+                str_start = tcp.data.find('{')
+                str_end = str(str_start).find('\r\n')
+                json_str = tcp.data[str_start:str_end]
+                print json_str
